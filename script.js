079a86b1eaf74774dcd9ca6239ddb1118f0ea493
Marcos <marcos.lprado@sempreceub.com>
2024-08-12 10:49:41 -0300

    feat: adding homework comment

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ submitButton.addEventListener("click", submit);
 function submit() {
     let elementName = document.getElementById("name").value;
     let elementAge = document.getElementById("age").value;
-    let elementEmail = document.getElementById("email").value;
+    let elementEmail = document.getElementById("email").value; // verificar se o email é válido (valid email regex/doc)
     console.log(`Name: ${elementName}\nAge: ${elementAge}\nEmail: ${elementEmail}`);
 }
