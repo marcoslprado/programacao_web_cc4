e6ab5fcfe98954cfe72f1e543b7b25dfdc986a9d
Marcos <marcos.lprado@sempreceub.com>
2024-08-16 18:31:21 -0300

    feat: creating responsivity and email validator using regex

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ let submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener("click", submit);
 
-function submit() {
+function submit(event) {
     let elementName = document.getElementById("name").value;
     let elementAge = document.getElementById("age").value;
-    let elementEmail = document.getElementById("email").value; // verificar se o email é válido (valid email regex/doc)
+    let elementEmail = document.getElementById("email").value;
+
+    // Validar o email
+    if (!validateEmail(elementEmail)) {
+        alert("Por favor, insira um e-mail válido.");
+        event.preventDefault(); // Evita que o formulário seja enviado
+        return; // Sai da função se o e-mail for inválido
+    }
+
     console.log(`Name: ${elementName}\nAge: ${elementAge}\nEmail: ${elementEmail}`);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^\w+.*@\w+(\.\w+)+$/;
+    return emailRegex.test(email);
 }
